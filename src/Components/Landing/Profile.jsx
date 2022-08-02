c35b9bce056437b9602d883304d9590bb3a28814
Apisit Tanapichartsakul <Apisit Tanapichartsakul>
2022-08-02 15:25:12 +0700

    update profile

--- src/Components/Landing/Profile.jsx
+++ src/Components/Landing/Profile.jsx
@@ -1,1 +1,15 @@
-import react from 'react';
+import React from 'react';
+import ProfilePic from '../ProfilePage/ProfilePic.jpg'
+
+function Profile() {
+    return (
+        <div>
+            <div>
+                <img src="{ProfilePic}" alt="" />
+                <h1>NAM PRATHAI</h1>
+                <p>คุณออกกำลังกายต่อเนื่องมาแล้ว:  9 วัน </p>
+                <p>ครั้งล่าสุดเมื่อ:  1 วันที่แล้ว </p>
+            </div>
+        </div>
+    )
+}

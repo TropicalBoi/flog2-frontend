8ac07c17f0dd80fa17478651667b778b58de10aa
TropicalBoi <tewprai.bualoi@gmail.com>
2022-08-21 00:35:28 +0700

    tew update

--- src/Components/Landing/Status.jsx
+++ src/Components/Landing/Status.jsx
@@ -1,21 +1,31 @@
-import React from 'react';
-import './Status.css'
+import React from "react";
+import "./Status.css";
 
-function Status() {
-    return (
-        <div className="temp">
-            <div className='Status'>
-                <div className='ActivityImg'>
-                    <p>hello</p>
-                </div>
-                <div className='ActivityData'>
-                    <p>hi</p>
-                </div>
-            </div>
+function Status(props) {
+  return (
+    <div className="temp">
+      <div className="Status">
+        <div className="ActivityImg">
+          <img src={props.status.img} />
         </div>
-
-
-    )
+        <div className="ActivityData">
+          <p>
+            {props.status.activities.map((activityDetail) => {
+              return (
+                <div>
+                  <p activityDetail={activityDetail}>
+                    {activityDetail.name},{activityDetail.duration} mins
+                  </p>
+                </div>
+              );
+            })}
+          </p>
+          <p>{props.status.msg}</p>
+          <p>{props.status.date}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Status;
